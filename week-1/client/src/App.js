c392035fc3af750c2b9e7bdbb497fddfbdf6c2db
unknown <rdogan8891gmail.com>
2022-04-14 16:23:49 +0200

    crud operations has been added

--- week-1/client/src/App.js
+++ week-1/client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
-const [backendData,setBackendData]=useState()
+const [movies,setMovies]=useState()
 
  useEffect(()=>{
    getdata()
@@ -13,17 +13,37 @@ async function getdata(){
   const response =await fetch("http://localhost:5000/api")
   const data=await response.json()
   console.log(data)
-  setBackendData(data)
+  setMovies(data)
 }
+async function deleteMovie(Pid){
+      const baseURL=`http://localhost:3001/movies/${Pid}`
+      await fetch(baseURL,{
+          method:"DELETE"
+      })
+      setMovies(movies.filter(mov =>mov.id!==Pid))
+   }
  
 return (
     <div className="App">
-    {backendData?.map((p,index) =>(
-      <div key={index}>
-        <p>{p.name}</p>
-        <p>{p.surname}</p>
+      <div className='row'>
+        
+        {movies?.map(movie =>(
+      <div className='col-lg-3 m-3'key={movie.id}>
+        <div className="card mb-4 shadow-sm" >
+          <img src={movie.imageURL} className="card-img-top picture" alt="..."/>
+          <div className="card-body">
+              <h5 className="card-title">{movie.name}</h5>
+              <p className="card-text">{movie.overview}</p>
+              <div className='d-flex justify-content-between align-items-center'>
+                <button onClick={()=> deleteMovie(movie.id)} type='button' className='btn btn-md btn-outline-danger'>DELETE</button>
+                <h2><span className='badge bg-info'>{movie.rating}</span></h2>
+              </div>
+          </div>
+          </div>
+       </div>
+     ))}
+       
       </div>
-    ))}
     </div>
   );
 }
